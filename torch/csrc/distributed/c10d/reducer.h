7abc845e6d5837020caa4feae90b0c204a13a56c
Rohan Varma <rvarm1@fb.com>
2020-08-17 14:34:36 -0700

    Join-based API to support DDP uneven inputs
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42577
    
    Closes https://github.com/pytorch/pytorch/issues/38174. Implements a join-based API to support training with the DDP module in the scenario where different processes have different no. of inputs. The implementation follows the description in https://github.com/pytorch/pytorch/issues/38174. Details are available in the RFC, but as a summary, we make the following changes:
    
    #### Approach
    1) Add a context manager `torch.nn.parallel.distributed.join`
    2) In the forward pass, we schedule a "present" allreduce where non-joined process contribute 1 and joined processes contribute 0. This lets us keep track of joined processes and know when all procs are joined.
    3) When a process depletes its input and exits the context manager, it enters "joining" mode and attempts to "shadow" the collective comm. calls made in the model's forward and backward pass. For example we schedule the same allreduces in the same order as the backward pass, but with zeros
    4) We adjust the allreduce division logic to divide by the effective world size (no. of non-joined procs) rather than the absolute world size to maintain correctness.
    5) At the end of training, the last joined process is selected to be the "authoritative" model copy
    
    We also make some misc. changes such as adding a `rank` argument to `_distributed_broadcast_coalesced` and exposing some getters/setters on `Reducer` to support the above changes.
    
    #### How is it tested?
    We have tests covering the following models/scenarios:
    - [x] Simple linear model
    - [x] Large convolutional model
    - [x] Large model with module buffers that are broadcast in the forward pass (resnet). We verify this with a helper function `will_sync_module_buffers` and ensure this is true for ResNet (due to batchnorm)
    - [x] Scenario where a rank calls join() without iterating at all, so without rebuilding buckets (which requires collective comm)
    - [x] Model with unused params (with find unused parameters=True)
    - [x] Scenarios where different processes iterate for a varying number of different iterations.
    - [x] Test consistency in tie-breaking when multiple ranks are the last ones to join
    - [x] Test that we divide by the effective world_size (no. of unjoined processes)
    
    
    #### Limitations
    1) This is only implemented for MPSD, not SPMD. Per a discussion with @mrshenli we want to encourage the use of MPSD over SPMD for DDP.
    2) This does not currently work with SyncBN or custom collective calls made in the model's forward pass. This is because the `join` class only shadows the `broadcast` for buffers in the forward pass, the gradient allreduces in the bwd pass, unused parameters reduction, and (optionally) the rebuild buckets broadcasting in the backwards pass. Supporting this will require additional design thought.
    3) Has not been tested with the [DDP comm. hook](https://github.com/pytorch/pytorch/issues/39272) as this feature is still being finalized/in progress. We will add support for this in follow up PRs.
    ghstack-source-id: 110090486
    
    Differential Revision: [D22893859](https://our.internmc.facebook.com/intern/diff/D22893859/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D22893859/)!

--- torch/csrc/distributed/c10d/reducer.h
+++ torch/csrc/distributed/c10d/reducer.h
@@ -17,6 +17,7 @@ namespace c10d {
 
 constexpr int kDefaultFirstBucketBytes = int(1024 * 1024);
 constexpr int kDefaultBucketBytesCap = int(25 * 1024 * 1024);
+constexpr int kUnsetDivFactor = -1;
 
 class Reducer {
  public:
@@ -59,17 +60,43 @@ class Reducer {
   // be called once before calling backward.
   void register_comm_hook(std::unique_ptr<CommHookInterface> iface);
 
+  // Returns a vector of tensors in each bucket in sequential order.
+  std::vector<std::vector<at::Tensor>> getBucketTensors() const;
+
+  // Rebuild buckets according to when tensors received gradients in the
+  // backward pass.
+  std::vector<std::vector<size_t>> rebuildBuckets();
+
+  // Returns true if we should rebuild buckets, else false. We only rebuild
+  // buckets once after the first iteration and never rebuild them if
+  // find_unused_parameters_.
+  bool shouldRebuildBuckets() const;
+
+  // Pushes all parameters to be rebuilt.
+  void pushRebuiltParamsForAllIndices();
+
+  // Creates and sets ForwardPassWorkHandle given a ProcessGroup::Work and the
+  // corresponding tensor being reduced.
+  void setForwardPassWorkHandle(
+      std::shared_ptr<c10d::ProcessGroup::Work> forwardPassWorkHandle,
+      at::Tensor& tensor);
+
+  // Retrieve on-device tensors used to track locally unused parameters. For
+  // each replica, it is a tensor where index i = 1 if the Variable with that
+  // index has been used.
+  std::vector<at::Tensor> getLocalUsedMapsOnDevice() const;
+
  protected:
   // Forward declaration.
   struct Bucket;
-
   // Locates a specific variable by replica index and variable index.
   struct VariableIndex {
     size_t replica_index;
     size_t variable_index;
   };
+  void pushRebuiltParams(const VariableIndex& index);
 
-  std::mutex mutex_;
+  mutable std::mutex mutex_;
   std::vector<std::vector<torch::autograd::Variable>> replicas_;
   std::shared_ptr<c10d::ProcessGroup> process_group_;
   std::vector<std::vector<bool>> expect_sparse_gradients_;
@@ -135,7 +162,6 @@ class Reducer {
   // and parameter indices order may change more frequently.
   // For find_unused_parameters = false case, buckets are only rebuilt once,
   // the performance cost is negligible.
-  std::vector<std::vector<size_t>> rebuildBuckets();
 
   using GradCallback =
       torch::distributed::autograd::DistAutogradContext::GradCallback;
@@ -153,7 +179,6 @@ class Reducer {
   // Buckets are filled as the gradients they hold are computed (triggered by
   // autograd hooks). Buckets are reduced in a predetemined order that is
   // identical across processes.
-  //
   struct BucketReplica {
     // Flattened (1 dimensional) contents of bucket.
     at::Tensor contents;
@@ -258,6 +283,18 @@ class Reducer {
   };
   RpcContext rpc_context_;
 
+  // A struct containing work handle and tensor for allreduce scheduled in
+  // forward pass, if applicable.
+  struct ForwardPassAllreduceWork {
+    std::shared_ptr<c10d::ProcessGroup::Work> workHandle;
+    at::Tensor resultTensor;
+  };
+
+  // Handle for the currently scheduled allreduce in the forward pass, if
+  // applicable.
+  ForwardPassAllreduceWork forwardPassWorkHandle_;
+  // Division factor for reduction of gradients.
+  int divFactor_{kUnsetDivFactor};
  private:
   // comm_hook_ is used to access the DDP communication hook if registered.
   std::unique_ptr<CommHookInterface> comm_hook_;
